bd184a2de35b83c8fdc6a87702189d2ac31cf885
GodKimba <rafaelfadel2@gmail.com>
2022-06-11 13:43:35 -0300

    created the mix project skeleton and started the game logic, creating the coordinate and island modules

--- islands_engine/mix.exs
+++ islands_engine/mix.exs
@@ -1,9 +1,9 @@
-defmodule KV.MixProject do
+defmodule IslandsEngine.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :kv,
+      app: :islands_engine,
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
@@ -14,7 +14,8 @@ defmodule KV.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {IslandsEngine.Application, []}
     ]
   end
 

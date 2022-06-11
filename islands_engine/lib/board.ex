fcb14e27dc8632fbdebdc5f82d0ba01d42a00d7f
GodKimba <rafaelfadel2@gmail.com>
2022-06-11 14:03:49 -0300

    created the board module

---
+++ islands_engine/lib/board.ex
@@ -0,0 +1,16 @@
+defmodule IslandsEngine.Board do
+  alias IslandsEngine.Coordinate
+
+  @letters ~W(a b c d e f g h i j)
+  @numbers [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  def start_link do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  defp keys() do
+    for letter <- @letters, number <- @numbers do
+      String.to_atom("#{letter}#{number}")
+    end
+  end
+end
